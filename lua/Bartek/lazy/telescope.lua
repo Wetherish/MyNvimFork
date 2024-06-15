e783ffd063f4ffd43294dc30bf3267e0dc85ec79
Wetherish <bartikozlowski@gmail.com>
2024-06-15 21:17:20 +0200

    telescope update

--- lua/Bartek/lazy/telescope.lua
+++ lua/Bartek/lazy/telescope.lua
@@ -8,10 +8,20 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup {
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "build"
+                }
+            }
+        }
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
@@ -27,4 +37,3 @@ return {
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
