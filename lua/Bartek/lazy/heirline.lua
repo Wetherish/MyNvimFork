44f4273602a50aab2db24c4a092b6baa03338bae
Wetherish <bartikozlowski@gmail.com>
2024-06-15 11:54:08 +0200

    MyNvim

---
+++ lua/Bartek/lazy/heirline.lua
@@ -0,0 +1,7 @@
+return{
+ 
+        "rebelot/heirline.nvim",
+        -- You can optionally lazy-load heirline on UiEnter
+        -- to make sure all required plugins and colorschemes are loaded before setup
+        -- event = "UiEnter",
+}
---
+++ lua/Bartek/lazy/toggleterm.lua
@@ -0,0 +1,4 @@
+return { "akinsho/toggleterm.nvim", config = true, cmd = "ToggleTerm", 
+keys = { { "<C-j>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } }, opts = { open_mapping = [[<C-j>]], direction = "float", shade_filetypes = {}, -- or
+  }}
+  
