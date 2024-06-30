da643e1896e7ade24ccfe0ae1ec000a4ff18a989
Wetherish <bartikozlowski@gmail.com>
2024-06-30 23:38:32 +0200

    git diff added

--- lua/Bartek/remap.lua
+++ lua/Bartek/remap.lua
@@ -38,16 +38,16 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>")
 vim.keymap.set(
     "n",
     "<leader>ee",
     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
-
+vim.keymap.set("n", "<leader>pd", "<cmd>DiffviewOpen<CR>")
+vim.keymap.set("n", "<leader>ph", "<cmd>DiffviewFileHistory<CR>")
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/Bartek/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
