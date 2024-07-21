b113a54f8ffc54bafd9abe7e322abc66d2087cc4
Bartosz Kozlowski <bartikozlowski@gmail.com>
2024-07-22 00:21:19 +0200

    update

--- lua/Bartek/lazy/colors.lua
+++ lua/Bartek/lazy/colors.lua
@@ -1,11 +1,11 @@
-return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            vim.cmd("colorscheme catppuccin-latte")
-        end
-    },
-
+return {{
+            "catppuccin/nvim",
+            name = "catppuccin",
+            config = function()
+                vim.cmd("colorscheme catppuccin-frappe")
+                vim.api.nvim_set_hl(0, "Normal", {bg = none} )
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            end
+        },
 
 }
--- lua/Bartek/remap.lua
+++ lua/Bartek/remap.lua
@@ -12,23 +12,22 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
+require("vim-with-me").StartVimWithMe()
 end)
 vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
+require("vim-with-me").StopVimWithMe()
 end)
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<C-c>", [["+y]])
+vim.keymap.set("n", "<C-C>", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -42,9 +41,9 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<C-q>", "<cmd>q<CR>")
 vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+"n",
+"<leader>ee",
+"oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
 vim.keymap.set("n", "<leader>pd", "<cmd>DiffviewOpen<CR>")
 vim.keymap.set("n", "<leader>ph", "<cmd>DiffviewFileHistory<CR>")
@@ -54,6 +53,16 @@ vim.keymap.set("n", "<A-Left>", "<cmd>tabprevious<CR>")
 vim.keymap.set("n", "<A-Right>", "<cmd>tabnext<CR>")
 vim.keymap.set("n", "<A-Up>", "<cmd>tabnew<CR>")
 vim.keymap.set("n", "<C-q>", "<cmd>q<CR>")
+-- Map Shift-Tab to un-indent in insert mode
+vim.keymap.set("i", "<S-Tab>", "<C-D>", { noremap = true, silent = true })
+
+-- Map Shift-Tab to un-indent in visual mode
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-a>", "<ggVG", { noremap = true, silent = true })
+vim.keymap.set({"n", "i"}, "<C-/>", "<cmd>Commentary<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-/>", "<Plug>Commentary", { noremap = true })
+vim.keymap.set('n', '<C-d>', 'yyp', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-r>', vim.lsp.buf.rename, { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
